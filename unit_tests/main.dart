3aa3d34d95c4fe0a09bdefac5f102a2bd7697517
Cathy Thomas <cathythomas@deloitte.co.uk>
2021-03-23 16:22:13 +0000

    KatUnitTestSetup

---
+++ unit_tests/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'lib/map_extension_test.dart' as map_extension;
+
+/// Unit tests
+void main() {
+  // group for the module
+  group('map_extension', () {
+    map_extension.main();
+  });
+}
